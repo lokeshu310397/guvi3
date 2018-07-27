edf4ba05c63789fdac3b8ebf3d21cadb5983208f
Upputuri Lokesh Babu <41730681+lokeshu310397@users.noreply.github.com>
2018-07-27 10:25:10 +0530

    Update even or odd.java

--- even or odd.java
+++ even or odd.java
@@ -12,13 +12,14 @@ class Ideone
 		int num;
 		Scanner input =new Scanner(System.in);
 		num=input.nextInt();
-		if(num%2==0)
+		if(num>0)
 		{
-			System.out.println("Even");
-		}
-		else
-		{
+		     if(num%2==0)
+		   	System.out.println("Even");
+		     else
 			System.out.println("Odd");
 		}
+		else
+		    System.out.println("Invalid");
 	}
 }
